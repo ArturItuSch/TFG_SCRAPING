5c545e27a69603574875d6294051d0d1a298c6c8
ArturSchuldt <artur.schuldt.iturri.al@iespoligonosur.org>
2025-04-25 11:21:05 +0200

    Team Info 1.0

--- miweb/scraping/Scripts/Leaguepedia/scraping_leaguepedia.py
+++ miweb/scraping/Scripts/Leaguepedia/scraping_leaguepedia.py
@@ -21,27 +21,45 @@ headers = {
 }
 
 def get_team_data(urls):
-    team_data = []
+    team_data = []  # Lista para almacenar los datos de los equipos
     for url in urls:
         try:
             response = requests.get(url, headers=headers)
             response.raise_for_status()  # Lanza un error si la respuesta no es 200
             soup = BeautifulSoup(response.content, 'html.parser')
-            
-            nombre = soup.find('h1', {'id': 'firstHeading'}).text.strip()
-            logo_img = soup.find('a', {'class': 'image'}).find('img')
-            logo_url = 'https:' + logo_img['src'] if logo_img else None
-            
+            table = soup.find('table', {'class': 'infobox InfoboxTeam'}, id='infoboxTeam')
+            if not table:
+                print(f"❌ No se encontró la tabla en {url}")
+                continue
+            else:
+                rows = table.find_all('tr')
+                
+                # Extraer datos de las filas de la tabla
+                for row in rows:
+                    cells = row.find_all('th')
+                    if len(cells) > 1:
+                        team_info = {
+                            '''"nombre": #Si el th class se llama infobox-title, entonces extraer el texto
+                                cells[0].text.strip() if 'infobox-title' not in cells[0]['class'] else cells[1].text.strip(),
+                            "ubicacion": # Si el td text es Org Location entonces extraer del td el primer span title
+                            "region": # Si el tr class se llama infobox-region, entonces extraer el texto del primer div
+                            "entrenador": #Si el td test es Head Coach entonces extraer todo el texto del td 
+                            "creacion": cells[4].text.strip()'''
+                        }
+                team_data.append(team_info)  # Añadir la información a la lista
+                print(f"✅ Datos extraídos de {url}")
+        except requests.HTTPError as e:
+            print(f"❌ Error HTTP: {e}")
         except requests.RequestException as e:
             print(f"❌ Error al realizar la solicitud: {e}")
         except Exception as e:
             print(f"❌ Error al procesar la página: {e}")
+    
     return team_data
 
 # Conseguir los enlaces de los equipos
 # de la LEC 2025 Spring Season desde Leaguepedia
-def get_team_links():
-    url = 'https://lol.fandom.com/wiki/LEC/2025_Season/Spring_Season'
+def get_team_links(url):
     base_url = 'https://lol.fandom.com/'
     response = requests.get(url, headers=headers)
     response.raise_for_status()
@@ -57,8 +75,11 @@ def get_team_links():
     return urls
 
 if __name__ == "__main__":
-    equipos = get_team_links()
+    equipos = get_team_links("https://lol.fandom.com/wiki/LEC/2025_Season/Spring_Season")  
 
     # Mostrar los resultados
     for equipo in equipos:
+        print(equipo)
+    team_data = get_team_data(equipos)
+    for equipo in team_data:
         print(equipo)
