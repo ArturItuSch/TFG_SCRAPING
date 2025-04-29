e08636c8f7b4c968dc2943ac6b38e576a0660565
ArturSchuldt <artur.schuldt.iturri.al@iespoligonosur.org>
2025-04-29 13:08:33 +0200

    Descarga Logo Equipos

--- miweb/scraping/Scripts/Leaguepedia/scraping_leaguepedia.py
+++ miweb/scraping/Scripts/Leaguepedia/scraping_leaguepedia.py
@@ -1,9 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import os
+import sys
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../'))
+sys.path.insert(0, PROJECT_ROOT)
 
-
-
+from Resources.rutas import CARPETA_IMAGENES_TEAMS
+TEAM_IMAGES_DIR = os.path.join(CARPETA_IMAGENES_TEAMS)
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
@@ -12,7 +16,7 @@ headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
-    "DNT": "1", # Do Not Track Request Header
+    "DNT": "1", 
     "Sec-Fetch-Dest": "document",
     "Sec-Fetch-Mode": "navigate",
     "Sec-Fetch-Site": "same-origin",
@@ -20,45 +24,8 @@ headers = {
     "Cache-Control": "max-age=0",
 }
 
-def get_team_data(urls):
-    team_data = []  # Lista para almacenar los datos de los equipos
-    for url in urls:
-        try:
-            response = requests.get(url, headers=headers)
-            response.raise_for_status()  # Lanza un error si la respuesta no es 200
-            soup = BeautifulSoup(response.content, 'html.parser')
-            table = soup.find('table', {'class': 'infobox InfoboxTeam'}, id='infoboxTeam')
-            if not table:
-                print(f"❌ No se encontró la tabla en {url}")
-                continue
-            else:
-                rows = table.find_all('tr')
-                
-                # Extraer datos de las filas de la tabla
-                for row in rows:
-                    cells = row.find_all('th')
-                    if len(cells) > 1:
-                        team_info = {
-                            '''"nombre": #Si el th class se llama infobox-title, entonces extraer el texto
-                                cells[0].text.strip() if 'infobox-title' not in cells[0]['class'] else cells[1].text.strip(),
-                            "ubicacion": # Si el td text es Org Location entonces extraer del td el primer span title
-                            "region": # Si el tr class se llama infobox-region, entonces extraer el texto del primer div
-                            "entrenador": #Si el td test es Head Coach entonces extraer todo el texto del td 
-                            "creacion": cells[4].text.strip()'''
-                        }
-                team_data.append(team_info)  # Añadir la información a la lista
-                print(f"✅ Datos extraídos de {url}")
-        except requests.HTTPError as e:
-            print(f"❌ Error HTTP: {e}")
-        except requests.RequestException as e:
-            print(f"❌ Error al realizar la solicitud: {e}")
-        except Exception as e:
-            print(f"❌ Error al procesar la página: {e}")
-    
-    return team_data
 
-# Conseguir los enlaces de los equipos
-# de la LEC 2025 Spring Season desde Leaguepedia
+# Conseguir los enlaces de los equipos de la LEC 2025 Spring Season desde Leaguepedia
 def get_team_links(url):
     base_url = 'https://lol.fandom.com/'
     response = requests.get(url, headers=headers)
@@ -74,12 +41,54 @@ def get_team_links(url):
     
     return urls
 
+def get_teams_logos(url):
+    # Realizar la solicitud HTTP
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print(f"Error al acceder a la página: {response.status_code}")
+        return
+
+    # Parsear el contenido HTML
+    soup = BeautifulSoup(response.content, 'html.parser')
+    tables = soup.find_all('table', {'class': 'wikitable tournament-roster'})
+    logos = []
+    nombre_equipos = []
+    
+    for table in tables:
+        # Obtener el nombre del equipo
+        nombre_equipo = table.find('tr').get_text(strip=True)
+        nombre_equipo = nombre_equipo.replace(' ', '_').replace('/', '_')  # Limpiar nombre
+        nombre_equipos.append(nombre_equipo)
+
+        # Obtener el logo
+        row = table.find('tr', {'class': 'RosterLogos'})
+        logo_url = row.find('img')
+        if logo_url:
+            logo = logo_url.get('data-src') or logo_url.get('src')
+            if logo:
+                logos.append(logo)
+
+    # Descargar las imágenes
+    for nombre_equipo, logo_url in zip(nombre_equipos, logos):
+        nombre_imagen = f"{nombre_equipo}_logo.png"
+        ruta_imagen = os.path.join(TEAM_IMAGES_DIR, nombre_imagen)
+
+        try:
+            print(f"Descargando {nombre_imagen}...")
+            img_data = requests.get(logo_url).content
+            with open(ruta_imagen, 'wb') as f:
+                f.write(img_data)
+            print(f"Imagen {nombre_imagen} descargada correctamente.")
+        except Exception as e:
+            print(f"Error al descargar la imagen {nombre_imagen}: {e}")
+            
 if __name__ == "__main__":
     equipos = get_team_links("https://lol.fandom.com/wiki/LEC/2025_Season/Spring_Season")  
 
     # Mostrar los resultados
-    for equipo in equipos:
-        print(equipo)
-    team_data = get_team_data(equipos)
-    for equipo in team_data:
-        print(equipo)
+    '''for equipo in equipos:
+        print(equipo)'''
+        
+    # Obtener los logos de los equipos
+    get_teams_logos("https://lol.fandom.com/wiki/LEC/2025_Season/Spring_Season")
+    
