fcfcbd4e62bd2d4d7e29a889f67e36cea4e57fe6
ArturSchuldt <artur.schuldt.iturri.al@iespoligonosur.org>
2025-06-13 10:13:05 +0200

    docs Finalizado

--- miweb/miweb/settings.py
+++ miweb/miweb/settings.py
@@ -27,7 +27,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(os.path.join(BASE_DIR, '.env.prd'))
+load_dotenv(os.path.join(BASE_DIR, '.env.dev'))
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 # Quick-start development settings - unsuitable for production
